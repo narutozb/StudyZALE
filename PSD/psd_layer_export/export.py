d44040ba23a46c192d87a28ff80758b6ca448e36
narutozb@hotmail.com <Qiaoyuanzhen_a03>
2022-10-03 19:08:54 +0900

    UI制作

--- PSD/psd_layer_export/export.py
+++ PSD/psd_layer_export/export.py
@@ -4,21 +4,82 @@ from psd_tools import PSDImage
 
 
 
-orig_ = r'D:\test_scenes\top\psd\test_psd.psd'
 
-orig_short_file_name, orig_suffix = os.path.splitext(os.path.basename(orig_))
-orig_parent_dir_path = Path(orig_).parent.parent.absolute()
-psd = PSDImage.open(orig_)
-save_file_type = 'png'
+def export_psd_group_to(orig_=None):
 
-save_file_prefix = ''
-save_file_suffix = ''
 
-for layer in psd:
-    if not layer.visible:
-        layer.visible = True
+    NORMAL_EXPORT_NAME_LIST = ['C', 'N', 'ORM', 'shellMask']
 
-    group_name = layer.name
-    save_path = os.path.join(orig_parent_dir_path,
-                             f'{save_file_prefix}{group_name}{save_file_suffix}.{save_file_type}')
-    layer.composite().save(save_path)
+
+    orig_short_file_name, orig_suffix = os.path.splitext(os.path.basename(orig_))
+    orig_parent_dir_path = Path(orig_).parent.parent.absolute()
+
+    psd = PSDImage.open(orig_)
+    save_file_type = 'png'
+
+    save_file_prefix = ''
+    save_file_suffix = ''
+
+    for layer in psd:
+        if not layer.visible:
+            layer.visible = True
+
+        group_name = layer.name
+        if group_name not in NORMAL_EXPORT_NAME_LIST:
+            continue
+
+        save_path = os.path.join(orig_parent_dir_path,
+                                 f'{save_file_prefix}{orig_short_file_name}_{group_name}{save_file_suffix}.{save_file_type}')
+        layer.composite().save(save_path)
+
+
+
+#------------------------------------------------
+# for root,dirs, files in os.walk(r'D:\test_scenes\top'):
+#     for file in files:
+#         short_name, suffix = os.path.splitext(file)
+#         if suffix == '.psd':
+#             orig_file_path = os.path.join(root,file)
+#             export_psd_group_to(orig_file_path)
+
+
+
+
+
+def get_psd_infos(orig_=None):
+
+
+    NORMAL_EXPORT_NAME_LIST = ['C', 'N', 'ORM', 'shellMask']
+    LAYER_INFOS_REFERENCE = {
+        'Name': [],
+        'IsVisible': [],
+        'Type?': [],
+        'Size?': [],
+        'ParentName?': [],
+        'HasMask?': [],
+        'HasEffect?': [],
+    }
+
+    psd = PSDImage.open(orig_)
+
+    for layer in psd:
+        group_name = layer.name
+        if group_name not in NORMAL_EXPORT_NAME_LIST:
+            continue
+        LAYER_INFOS_REFERENCE.get('Name').append(layer.name)
+        LAYER_INFOS_REFERENCE.get('IsVisible').append(str(layer.is_visible()))
+        LAYER_INFOS_REFERENCE.get('Type?').append(layer.kind)
+        # 如果不显示图层则无法判断图层大小
+        if not layer.visible:
+            layer.visible = True
+        LAYER_INFOS_REFERENCE.get('Size?').append(layer.size)
+        LAYER_INFOS_REFERENCE.get('ParentName?').append(layer.parent.name)
+        LAYER_INFOS_REFERENCE.get('HasMask?').append(layer.has_mask())
+        LAYER_INFOS_REFERENCE.get('HasEffect?').append(layer.has_effects())
+
+    return LAYER_INFOS_REFERENCE
+
+
+
+
+
--- PSD/psd_layer_export/tablewidget_samples.py
+++ PSD/psd_layer_export/tablewidget_samples.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pandas as pd
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QTableWidget, QTableWidgetItem, QApplication, QHeaderView, \
@@ -5,58 +6,137 @@ from PySide6.QtWidgets import QWidget, QHBoxLayout, QTableWidget, QTableWidgetIt
     QLineEdit, QSpinBox, QPushButton, QFileDialog
 from PySide6.QtCore import QItemSelection, QLocale, Qt, Slot
 
-# from typing import Optional
+from export import get_psd_infos
 from numbers import Number
 
 #
 LAYER_DETAIL_INFO_LIST = ['Name', 'IsVisible', 'Type?', 'Size?', 'ParentName?', 'HasMask?', 'HasEffect?']
 
-LAYER_INFOS_REFERENCE = {
-    'Name': ['GroupA', 'GroupB'],
-    'IsVisible': [True, False],
-    'Type?': ['Group', 'Group'],
-    'Size?': ['1000*1000', '1000*1000'],
-    'ParentName?': ['Root', 'Root'],
-    'HasMask?': [False, False],
-    'HasEffect?': [False, False],
-}
-df = pd.DataFrame(data=LAYER_INFOS_REFERENCE)
-df_t = df.T
-
 
 class LayertableWidget(QWidget):
     def __init__(self) -> None:
         super().__init__()
+
+        # self.setFixedSize(800,500)
+        self.resize(800, 500)
+
         self.init_ui()
         self.set_file_list_ui()
-        self.set_layers_ui(2, 7)
+        self.set_layers_ui()
 
         main_layout = QVBoxLayout()
         main_layout.addWidget(self._form_group_box)
-        main_layout.addWidget(self.layer_table_widget)
+        main_layout.addWidget(self._gb_layer_detail_box)
         self.setLayout(main_layout)
 
     def init_ui(self):
         # 设置标题
         self.setWindowTitle('LayerInfos')
 
-    def set_layers_ui(self, rows, cols):
-        layer_detail_layout = QHBoxLayout()
-        # 建立QTabWidget布局
-        self.layer_table_widget = QTableWidget(rows, cols)
+    def set_layers_ui(self):
+        self._gb_layer_detail_box = QGroupBox('Layer Details')
+        layout = QVBoxLayout()
 
-        layer_detail_layout.addWidget(self.layer_table_widget)
+        # 建立QTabWidget布局
+        self.layer_table_widget = QTableWidget(0,len(LAYER_DETAIL_INFO_LIST))
+
+
+        self._gb_layer_detail_box.setLayout(layout)
         self.layer_table_widget.width = 2000
-        self.layer_table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        self.layer_table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.layer_table_widget.verticalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
 
         self.layer_table_widget.setHorizontalHeaderLabels(LAYER_DETAIL_INFO_LIST)  # set
         self.layer_table_widget.verticalHeader().hide()  # 隐藏index0
 
+        # 保存信息按钮
+        details_func_layout = QHBoxLayout()
+        bt_save_details = QPushButton('Save PSD layer details')
+        # details_func_layout.addWidget(bt_save_details)
+
+        # layout
+        layout.addWidget(self.layer_table_widget)
+        layout.addLayout(details_func_layout)
+
+
+    def set_file_list_ui(self):
+
+        self._form_group_box = QGroupBox("File list")
+        layout = QVBoxLayout()
+        bt_layout = QHBoxLayout()
+
+
+        bt_add_file_to_list = QPushButton('Add PSD Files')
+        bt_remove_file = QPushButton('Remove File')
+        bt_export_all_list_file = QPushButton('Export All To')
+        # bt_export_all_list_file_to = QPushButton('Export All To')
+        # 绑定 在文件列表中移除文件 按钮
+        bt_remove_file.clicked.connect(self.remove_file_from_list_func)
+
+        # 绑定 添加文件名到文件列表
+        bt_add_file_to_list.clicked.connect(self.add_file_to_list_func)
+
+        self._list_widget = QListWidget()
+        self._list_widget.doubleClicked.connect(self.double_clicked_file_item_func)
+
+        export_layout = QHBoxLayout()
+
+        bt_layout.addWidget(bt_add_file_to_list)
+        bt_layout.addWidget(bt_remove_file)
+
+        export_layout.addWidget(bt_export_all_list_file)
+        # Layout
+        layout.addLayout(bt_layout)
+        layout.addWidget(self._list_widget)
+
+        layout.addLayout(export_layout)
+        self._form_group_box.setLayout(layout)
+
+    def remove_file_from_list_func(self)->None:
+        if not self._list_widget.currentItem():
+            return
+        if self._gb_layer_detail_box.title() == self._list_widget.currentItem().text():
+            self.layer_table_widget.setRowCount(0)
+            self._gb_layer_detail_box.setTitle('Select a file for Layer Details')
+        # remove func
+        self._list_widget.takeItem(self._list_widget.currentRow())
+
+
+
+    def add_file_to_list_func(self):
+        '''
+        获取所选择的文件列表，并且更新列表到相关UI
+        :return:
+        '''
+        file_names, selected_filter = \
+            QFileDialog.getOpenFileNames(self, caption='Open File', dir=os.path.expanduser('~'),
+                                         filter='psd files(*.psd *.psb)')
+
+
+        # 获取当前文件列表中列表
+        items_text = [self._list_widget.item(num).text() for num in range(self._list_widget.count())]
+
+        # 对比，并且更新列表
+        add_to = [i for i in file_names if i not in items_text]
+
+        self._list_widget.addItems(add_to)
+
+    def double_clicked_file_item_func(self):
+        current_file_name = self._list_widget.currentItem().text()
+        self._gb_layer_detail_box.setTitle('%s'%current_file_name)
+        dict1 = get_psd_infos(current_file_name)
+
+        # 将字典转化为 DataFrame. 需要numpy
+        df = pd.DataFrame(data=dict1)
+        df_t = df.T
+        # 初始化行
+        self.layer_table_widget.setRowCount(0)
+        self.layer_table_widget.setRowCount(len(dict1[list(dict1.keys())[0]]))
+
+
         for i in df_t:
             row = 0
             for j in df_t[i]:
-                print(f'({i}, {row}, QTableWidgetItem("{j}"))')
                 item = QTableWidgetItem(str(j))
                 # 如果列数大于某一行时，不可编辑
                 if row > 1:
@@ -64,28 +144,10 @@ class LayertableWidget(QWidget):
                 self.layer_table_widget.setItem(i, row, item)
                 row += 1
 
-    def set_file_list_ui(self):
+    # def export_all_file_to_func(self):
 
-        self._form_group_box = QGroupBox("Form layout")
-        layout = QVBoxLayout()
-        bt_layout = QHBoxLayout()
 
-        layout.addLayout(bt_layout)
-        button = QPushButton('AAA')
-        button2 = QPushButton('BBB')
-        _list_widget = QListWidget()
-        _list_widget.addItem("C++")
-        _list_widget.addItem("Java")
-        _list_widget.addItem("Python")
-        bt_layout.addWidget(button)
-        bt_layout.addWidget(button2)
-        layout.addWidget(_list_widget)
 
-        self._form_group_box.setLayout(layout)
-
-def get_files(parent):
-    file_name, selected_filter = \
-    QFileDialog.getOpenFileNames(parent, caption='Open File', dir=os.path.expanduser('~'))
 
 
 
